c44937b69f0e9dd03ebd18185864e4363f8891c0
XianJialu <929670649@qq.com>
2026-01-27 22:14:15 +0800

    template

--- packages/cli/src/templates/readme_template_v1.ts
+++ packages/cli/src/templates/readme_template_v1.ts
@@ -1,6 +1,4 @@
-export const getReadmeTemplate = (
-  projectName: string
-): string => `# ${projectName}
+export const getReadmeTemplate = (projectName: string): string => `# ${projectName}
 
 MCP server built with LeanMCP SDK.
 
